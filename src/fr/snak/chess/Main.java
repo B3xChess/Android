efd090afa056676bbf45d22fba9c35a3a9876bd2
Samak95 <nouletsylvain@hotmail.fr>
2016-03-09 18:53:14 +0100

    Mise en place de View

--- src/fr/snak/chess/Main.java
+++ src/fr/snak/chess/Main.java
@@ -1,16 +1,41 @@
 package fr.snak.chess;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import fr.snak.chess.View.Game;
 
-public class Main extends Activity {
+public class Main extends Activity implements View.OnClickListener {
     /**
      * Called when the activity is first created.
      */
+    private Button buttonGame;
+    private Button buttonSettings;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        buttonGame = (Button) findViewById(R.id.buttonGame);
+        buttonGame.setOnClickListener(this);
+
+        buttonSettings = (Button) findViewById(R.id.buttonSettings);
+        buttonSettings.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if( v == buttonGame ) {
+            Intent intent = new Intent(this, Game.class);
+            startActivity(intent);
+        }
+
+        if( v == buttonSettings ){
+            Toast.makeText(getApplicationContext(), "Not dev yet", Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ src/fr/snak/chess/View/Game.java
@@ -0,0 +1,16 @@
+package fr.snak.chess.View;
+
+import android.app.Activity;
+import android.os.Bundle;
+import fr.snak.chess.R;
+
+
+/**
+ * Created by sylvain on 09/03/2016.
+ */
+public class Game extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+}
